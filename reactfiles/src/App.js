a69bdda1a55d4afd4e0bdc448e0afefa5ee421d8
unknown <hasanhameed858@gmail.com>
2024-02-26 12:48:18 +0530

    first commit

---
+++ reactfiles/src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from './componenets/ExoenseItem.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <ExpenseItem>
+        </ExpenseItem>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ reactfiles/src/componenets/ExoenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h1>ExpenseItem</h1>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movies Rs 200</div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
